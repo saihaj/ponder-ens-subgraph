c79344c5f4f7fd7d8fecdd4d3ca5afdc72c90f84
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2024-03-05 13:45:28 -0500

    style: prettier

--- src/Resolver.ts
+++ src/Resolver.ts
@@ -4,13 +4,19 @@ ponder.on("Resolver:AuthorisationChanged", async ({ event, context }) => {
   console.log(event.args);
 });
 
-ponder.on("Resolver:TextChanged(bytes32 indexed node, string indexed indexedKey, string key, string value)", async ({ event, context }) => {
-  console.log(event.args);
-});
+ponder.on(
+  "Resolver:TextChanged(bytes32 indexed node, string indexed indexedKey, string key, string value)",
+  async ({ event, context }) => {
+    console.log(event.args);
+  },
+);
 
-ponder.on("Resolver:TextChanged(bytes32 indexed node, string indexed indexedKey, string key)", async ({ event, context }) => {
-  console.log(event.args);
-});
+ponder.on(
+  "Resolver:TextChanged(bytes32 indexed node, string indexed indexedKey, string key)",
+  async ({ event, context }) => {
+    console.log(event.args);
+  },
+);
 
 ponder.on("Resolver:PubkeyChanged", async ({ event, context }) => {
   console.log(event.args);
