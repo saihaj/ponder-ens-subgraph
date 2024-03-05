49c95fda141b81bbe55f3efdddac841e092ef17b
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2024-03-05 13:44:40 -0500

    fix handler

--- src/Resolver.ts
+++ src/Resolver.ts
@@ -4,11 +4,11 @@ ponder.on("Resolver:AuthorisationChanged", async ({ event, context }) => {
   console.log(event.args);
 });
 
-ponder.on("Resolver:TextChanged", async ({ event, context }) => {
+ponder.on("Resolver:TextChanged(bytes32 indexed node, string indexed indexedKey, string key, string value)", async ({ event, context }) => {
   console.log(event.args);
 });
 
-ponder.on("Resolver:TextChanged", async ({ event, context }) => {
+ponder.on("Resolver:TextChanged(bytes32 indexed node, string indexed indexedKey, string key)", async ({ event, context }) => {
   console.log(event.args);
 });
 
